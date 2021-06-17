81477226440a8442bec58977e87da38cea3a7c29
Andrea Fiori <andrea.fiori.1998@gmail.com>
2021-06-17 21:22:55 +0200

    remove useless files

--- util.h
+++
@@ -1,6 +0,0 @@
-#ifndef yala_util_h
-#define yala_uitl_h
-
-void varperror(char *fmt, ...);
-
-#endif
--- yala.c
+++ yala.c
@@ -1,8 +1,20 @@
+#include <errno.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#include "util.h"
 #include "frontend/frontend.h"
+
+void
+varperror(char *fmt, ...)
+{
+        va_list args;
+        va_start(args, fmt);
+        vfprintf(stderr, fmt, args);
+        fprintf(stderr, ": %s\n", strerror(errno));
+        va_end(args);
+}
 
 char *
 load_program(char *progname, char *fname, int *proglen)
