506b6ea0bf1c937f258adae22c0571ec54e7a62f
Andrea Fiori <andrea.fiori.1998@gmail.com>
2021-06-23 19:34:34 +0200

    make hash table

--- datastructs/datastructs.c
+++ datastructs/datastructs.c
@@ -4,6 +4,8 @@
 #include "datastructs.h"
 #include "../semantics/semantics.h"
 
+/* list */
+
 #define LIST_DEFINE(name, type) \
         LIST_INIT(name, type) \
         LIST_PUSH(name, type) \
@@ -74,3 +76,115 @@ grow_array(void *buffer, int oldcap, int newcap, size_t size)
 LIST_DEFINE(bytes, uint8_t)
 LIST_DEFINE(linelist, struct lineinfo)
 LIST_DEFINE(valuelist, struct value)
+
+/* hash table */
+
+#define TABLE_RESIZE_THRESHOLD 1
+
+#define TABLE_GROW_CAP(cap) ((cap) < 100 ? 100 : (int) (2 * (cap)))
+
+#define TABLE_DEFINE(name, keytype, valtype) \
+        TABLE_INIT(name, keytype, valtype) \
+        TABLE_TABLE_RESIZE(name, keytype, valtype) \
+        TABLE_TABLE_NODE(name, keytype, valtype) \
+        TABLE_HAS(name, keytype, valtype) \
+        TABLE_GET(name, keytype, valtype) \
+        TABLE_SET(name, keytype, valtype) \
+        TABLE_FREE(name, keytype, valtype)
+
+#define TABLE_INIT(name, keytype, valtype) \
+        void name##_init(struct name *table, int (*hash)(keytype), int (*keyequal)(keytype, keytype)) \
+        { \
+                table->cap = table->nnodes = 0; \
+                table->buckets = NULL; \
+                table->hash = hash; \
+                table->keyequal = keyequal; \
+        }
+
+#define TABLE_TABLE_RESIZE(name, keytype, valtype) \
+        void name##_table_resize(struct name *table) \
+        { \
+                int newcap = TABLE_GROW_CAP(table->cap); \
+                struct name##_table_node **newbuckets; \
+                newbuckets = calloc(newcap, sizeof(struct name##_table_node *)); \
+                int i, index; \
+                for (i = 0; i < table->cap; i++) { \
+                        struct name##_table_node *node, *next; \
+                        node = table->buckets[i]; \
+                        while (node != NULL) { \
+                                index = (*table->hash)(node->key) % newcap; \
+                                next = node->next; \
+                                node->next = newbuckets[index]; \
+                                newbuckets[index] = node; \
+                                node = next; \
+                        } \
+                } \
+                if (table->buckets != NULL) \
+                        free(table->buckets); \
+                table->buckets = newbuckets; \
+                table->cap = newcap; \
+        }
+
+#define TABLE_TABLE_NODE(name, keytype, valtype) \
+        struct name##_table_node *name##_node_get(struct name *table, keytype key) \
+        { \
+                struct name##_table_node *node = table->cap == 0 ? NULL : table->buckets[(*(table->hash))(key) % table->cap]; \
+                while (node != NULL) { \
+                        if ((*table->keyequal)(key, node->key)) { \
+                                break; \
+                        } \
+                        node = node->next; \
+                } \
+                return node; \
+        }
+
+
+#define TABLE_HAS(name, keytype, valtype) \
+        int name##_has(struct name *table, keytype key) \
+        { \
+                struct name##_table_node *node = name##_node_get(table, key); \
+                return node != NULL; \
+        }
+
+#define TABLE_GET(name, keytype, valtype) \
+        valtype name##_get(struct name *table, keytype key) \
+        { \
+                struct name##_table_node *node = name##_node_get(table, key); \
+                return node->value; \
+        }
+
+#define TABLE_SET(name, keytype, valtype) \
+        int name##_set(struct name *table, keytype key, valtype value) \
+        { \
+                if (name##_has(table, key)) \
+                        return 0; \
+                table->nnodes++; \
+                if (table->cap == 0 || (double) table->nnodes / (double) table->cap > TABLE_RESIZE_THRESHOLD) { \
+                        name##_table_resize(table); \
+                } \
+                int index = (*table->hash)(key) % table->cap; \
+                struct name##_table_node *node = malloc(sizeof(struct name##_table_node)); \
+                node->next = table->buckets[index]; \
+                node->key = key; \
+                node->value = value; \
+                table->buckets[index] = node; \
+                return 1; \
+        }
+
+
+#define TABLE_FREE(name, keytype, valtype) \
+        void name##_free(struct name *table) \
+        { \
+                struct name##_table_node *node, *next; \
+                int i; \
+                for (i = 0; i < table->cap; i++) { \
+                        node = table->buckets[i]; \
+                        while (node != NULL) { \
+                                next = node->next; \
+                                free(node); \
+                                node = next; \
+                        } \
+                } \
+                if (table->buckets != NULL) \
+                        free(table->buckets); \
+        }
--- datastructs/datastructs.h
+++ datastructs/datastructs.h
@@ -15,4 +15,24 @@
         int name##_len(struct name *list); \
         void name##_free(struct name *list);
 
+#define TABLE_DECLARE(name, keytype, valtype) \
+        struct name##_table_node { \
+                keytype key; \
+                valtype value; \
+                struct name##_table_node *next; \
+        }; \
+        struct name { \
+                int cap; \
+                int nnodes; \
+                struct name##_table_node **buckets; \
+                int (*hash)(keytype); \
+                int (*keyequal)(keytype, keytype); \
+        }; \
+        void name##_init(struct name *table, int (*hash)(keytype), int (*keyequal)(keytype, keytype)); \
+        struct name##_table_node *name##_node_get(struct name *table, keytype key); \
+        int name##_has(struct name *table, keytype key); \
+        valtype name##_get(struct name *table, keytype key); \
+        int name##_set(struct name *table, keytype key, valtype value); \
+        void name##_free(struct name *list);
+
 #endif
