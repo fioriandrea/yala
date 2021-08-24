7c2653474e2b0885e5f37adebeea3e0855201165
Andrea Fiori <andrea.fiori.1998@gmail.com>
2021-08-24 22:09:02 +0200

    remove useless code

--- semantics/value.c
+++ semantics/value.c
@@ -7,20 +7,6 @@
 
 /* list */
 
-#define LIST_DECLARE(name, type) \
-        struct name \
-        { \
-                int len; \
-                int cap; \
-                type *buffer; \
-        }; \
-        void name##_init(struct name *list); \
-        int name##_push(struct name *list, type data); \
-        type name##_pop(struct name *list); \
-        type name##_at(struct name *list, int i); \
-        int name##_len(struct name *list); \
-        void name##_free(struct name *list);
-
 #define LIST_DEFINE(name, type) \
         LIST_INIT(name, type) \
         LIST_PUSH(name, type) \
