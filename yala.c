87d39bf8237a8ed796a7eb0ded92306fc14d5ba6
Andrea Fiori <andrea.fiori.1998@gmail.com>
2021-09-01 11:42:40 +0200

    add first version of command line arguments

--- yala.c
+++ yala.c
@@ -9,7 +9,74 @@
 #include "semantics/semantics.h"
 #include "vm/vm.h"
 
-void
+static void varperror(char *fmt, ...);
+static void progvarperror(char *fmt, ...);
+static void progerror(char *fmt, ...);
+static void run_run(char *programtext, int proglen);
+
+enum run_mode {
+        RUN_RUN,
+        RUN_COMPILE,
+        RUN_EXECUTE,
+};
+
+struct run_mode_string {
+        char *str;
+        enum run_mode rm;
+};
+
+struct run_mode_string run_mode_strings[] = {
+        {"run", RUN_RUN},
+        {"compile", RUN_COMPILE},
+        {"execute", RUN_EXECUTE},
+        {NULL, 0},
+};
+
+static char *progname = "";
+static int display_tree;
+static int display_bytecode;
+static int run_mode;
+static char *input_path;
+
+static void
+parse_cli_arguments(int argc, char **argv)
+{
+        progname = *argv++;
+        argc--;
+        if (argc == 0) {
+                progerror("must supply mode\n");
+                exit(1);
+        }
+        char *run_mode_str = *argv++;
+        argc--;
+        for (struct run_mode_string *rms = run_mode_strings; ; rms++) {
+                if (rms->str == NULL) {
+                        progerror("unrecognized mode %s\n", run_mode_str);
+                        exit(1);
+                }
+                if (strcmp(rms->str, run_mode_str) == 0) {
+                        run_mode = rms->rm;
+                        break;
+                }
+        }
+        while (argc > 0 && (*argv)[0] == '-' && (*argv)[1] == '-') {
+                char *option = *argv++;
+                argc--;
+                if (strcmp(option, "--display-tree") == 0) {
+                        display_tree = 1;
+                } else if (strcmp(option, "--display-bytecode") == 0) {
+                        display_bytecode = 1;
+                }
+        }
+        if (argc == 0) {
+                progerror("must supply a file\n");
+                exit(1);
+        }
+        input_path = *argv++;
+        argc--;
+}
+
+static void
 varperror(char *fmt, ...)
 {
         va_list args;
@@ -19,8 +86,28 @@ varperror(char *fmt, ...)
         va_end(args);
 }
 
-char *
-load_program(char *progname, char *fname, int *proglen)
+static void
+progerror(char *fmt, ...)
+{
+        va_list args;
+        va_start(args, fmt);
+        fprintf(stderr, "%s: ", progname);
+        vfprintf(stderr, fmt, args);
+        va_end(args);
+}
+
+static void
+progvarperror(char *fmt, ...)
+{
+        va_list args;
+        va_start(args, fmt);
+        fprintf(stderr, "%s: ", progname);
+        varperror("");
+        va_end(args);
+}
+
+static char *
+load_program(char *fname, int *proglen)
 {
         FILE *fp;
         size_t fsize;
@@ -28,7 +115,7 @@ load_program(char *progname, char *fname, int *proglen)
 
         fp = fopen(fname, "r");
         if (fp == NULL) {
-                varperror("%s: cannot open file '%s'", progname, fname);
+                progvarperror("cannot open file '%s'", fname);
                 exit(1);
         }
         fseek(fp, 0, SEEK_END);
@@ -36,11 +123,11 @@ load_program(char *progname, char *fname, int *proglen)
         fseek(fp, 0, SEEK_SET);
         programtext = malloc(fsize + 1);
         if (programtext == NULL) {
-                varperror("%s: cannot allocate enough memory for file '%s'", progname, fname);
+                progvarperror("cannot allocate enough memory for file '%s'", fname);
                 exit(1);
         }
         if (fread(programtext, 1, fsize, fp) != fsize) {
-                varperror("%s: cannot read whole file '%s'", progname, fname);
+                progvarperror("cannot read whole file '%s'", fname);
                 exit(1);
         }
         fclose(fp);
@@ -49,27 +136,19 @@ load_program(char *progname, char *fname, int *proglen)
         return programtext;
 }
 
-int
-main(int argc, char **argv)
+static void
+run_run(char *programtext, int proglen)
 {
-        char *progname;
-        char *programtext;
-        int proglen;
         struct tree_node *root;
         struct bytecode *code;
         struct vm vm;
 
-        progname = *argv;
-        if (argc < 2) {
-                fprintf(stderr ,"usage: %s [OPTIONS] FILE\n", progname);
-                exit(1);
-        }
-        programtext = load_program(progname, argv[1], &proglen);
-
         root = parse(programtext, proglen);
         if (!root)
                 exit(1);
-        tree_node_print(root);
+
+        if (display_tree)
+                tree_node_print(root);
 
         code = generate_bytecode(root);
         if (code == NULL)
@@ -78,10 +157,34 @@ main(int argc, char **argv)
         tree_node_free(root);
         free(programtext);
 
-        disassemble(code);
+        if (display_bytecode)
+                disassemble(code);
 
         vm_init(&vm, code);
         vm_run(&vm);
+}
+
+int
+main(int argc, char **argv)
+{
+        char *programtext;
+        int proglen;
+
+        parse_cli_arguments(argc, argv);
+
+        programtext = load_program(input_path, &proglen);
+
+        switch (run_mode) {
+                case RUN_RUN:
+                        run_run(programtext, proglen);
+                        break;
+                case RUN_COMPILE:
+
+                break;
+                case RUN_EXECUTE:
+                
+                break;
+        }
 
         return 0;
 }
